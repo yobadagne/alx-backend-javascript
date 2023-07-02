8581f2b9900e13055ebe1c8071ebd4abd95e116c
yobadagne <yobadagne@gmail.com>
2023-07-02 17:38:22 +0000

    8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,12 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    // Create objs
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](type) {
+    if (type === 'string') return this._location;
+    return this._size;
+  }
+}
