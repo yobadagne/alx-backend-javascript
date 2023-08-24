ede2bc1cbdf942d35ee25ed74c5297ba0f64fb10
yobadagne <yobadagne@gmail.com>
2023-08-24 13:07:51 +0000

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout..write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
 	const name = process.stdin.read();
